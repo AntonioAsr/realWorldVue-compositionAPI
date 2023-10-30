f08ecac8ae82a46f239fc09591395a6546da6f9a
sego <antoniosegoviaruiz@gmail.com>
2023-10-30 17:53:54 +0100

    Lesson 5 - Api calls with axios

--- src/views/EventListView.vue
+++ src/views/EventListView.vue
@@ -1,41 +1,15 @@
 <script setup>
 import EventCard from '@/components/EventCard.vue';
-import { ref } from 'vue';
-const events = ref([
-    {
-      id: 234234,
-      category: 'origami convention',
-      title: 'Fold That',
-      description: 'Starting journey for new folders',
-      location: 'Brighton',
-      date: 'January 4, 2024',
-      time: '12:00',
-      petsAllowed: true,
-      organizer: 'Satoshi Kamiya'
-    },
-    {
-      id: 5928101,
-      category: 'animal welfare',
-      title: 'Cat Adoption Day',
-      description: 'Find your new feline',
-      location: 'London',
-      date: 'January 28, 2023',
-      time: '12:00',
-      petsAllowed: true,
-      organizer: 'John Scott'
-    },
-    {
-      id: 4428101,
-      category: 'I.T.',
-      title: 'Js for All',
-      description: 'code your first app using just js',
-      location: 'Madrid',
-      date: 'March 7, 2023',
-      time: '15:00',
-      petsAllowed: false,
-      organizer: 'Addi Osmani'
-    },
-    ],)
+import EventService from '@/services/EventService.js';
+import { ref, onMounted } from 'vue';
+const events = ref(null)
+
+onMounted(() => {
+  EventService.getEvents()
+    .then((response) => events.value = response.data)
+    .catch((error) => console.error(error))
+})
+
 </script>
 
 <template>
